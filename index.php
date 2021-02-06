f84546397775161ac35ecc83fec1ae9f9a938904
bagoes <bagoessoemargo@gmail.com>
2021-02-06 13:12:26 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis voluptatem perspiciatis, veniam vitae officiis, neque omnis blanditiis nisi fugiat reprehenderit iste ipsam natus, voluptate velit eius. A culpa cum perspiciatis?</p>
+    <ul>
+    <li>satu</li>
+    <li>dua</li>
+    </ul>
 </body>
 </html>
